ff4ed4785308d7a6341efd9b6a587b9ee4f37592
franckonline <vidoc89@gmail.com>
2021-09-01 15:52:15 +0200

    gestion api

--- js/index.js
+++ js/index.js
@@ -5,3 +5,4 @@ document.querySelector(".btnandco.btn1").addEventListener("click", function (){
 document.querySelector(".btnandco.btn2").addEventListener("click", function (){
     window.location.href = "inscription.html";
 });
+
