c07cbb19979f26fca9e2ea42adee768db5d63961
franckonline <vidoc89@gmail.com>
2021-09-01 16:54:15 +0200

    expression regulière

--- js/connect.js
+++ js/connect.js
@@ -1,9 +1,37 @@
-
-
+let bouton = document.querySelector("button");
+console.log("mon bouton", bouton);
  //  let inscript = document.querySelector(".btn_inscription");
 //  document.querySelector(".btn_inscription").addEventListener("click", function () {
 //      window.location.href = "inscription.html";
 //  });
+let champsmail = document.querySelector("#inp_mail");
+console.log("input mail",champsmail.value );
+
+let regex = new RegExp("/\\S+@\\S+\\.\\S+/");
+
+let champspass = document.querySelector("#inp_pass");
+console.log("input pass",champspass.value );
+
+champsmail.addEventListener("input", checkpass);
+
+function checkpass(){
+    let mailmail = this.value;
+    if (regex.test(mailmail)) {
+        // active button
+        bouton.removeAttribute("disabled");
+    }}
+    
+
+
+
+
+
+
+
+
+
+
+
 
  let data = {
     username: 'franck@example.com',
