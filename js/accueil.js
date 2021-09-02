ec7ee9b225518407b384d5cf8d23b9f57e446224
franckonline <vidoc89@gmail.com>
2021-09-02 16:48:51 +0200

    slide fetch

--- js/accueil.js
+++ js/accueil.js
@@ -1,20 +1,39 @@
 let token = sessionStorage.brouettetouk;
-let voirvingt ;
+let voirvingt;
+let count = 0;
+let tableaujacket = [];
 // console.log("mon token est : ", token);
-    
-fetch(`http://musics.logikstik.odns.fr/api/albums/?order[created_at]=desc`, {
-    method: 'GET',
-    
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        'Authorization': `Bearer ${token}`
-    }
 
-})
+fetch(`http://musics.logikstik.odns.fr/api/albums/?order[created_at]=desc`, {
+        method: 'GET',
+
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer ${token}`
+        }
+
+    })
     .then((response) => response.json())
-    
+
     .then((json) => {
         voirvingt = json;
         console.log("les albums ", voirvingt);
-       
+        for (count = 0; count < 20; count++) {
+            console.log("count value ", count);
+            tableaujacket[count] = voirvingt[count].picture;
+            // console.log("picture : ", tableaujacket[count]);
+            let jackette = document.querySelector(".templatefill");
+            let defiler = document.querySelector(".defill_h");
+
+            const clone = document.importNode(jackette.content, true);
+            console.log("essai jack ", clone);
+            const divslide = clone.querySelector(".slide");
+            console.log("slide ", divslide);
+            const imagejackette = clone.querySelector(".img_slide");
+            console.log("image jackou ", imagejackette);
+            imagejackette.src = tableaujacket[count];
+            console.log("numero image ",imagejackette);
+            console.log("imagejackette", imagejackette);
+            defiler.appendChild(clone);
+        }
     });
