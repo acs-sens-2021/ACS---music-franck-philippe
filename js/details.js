4a6b59b4050954898bd6d0c8e744a34b357ad2f4
franckonline <vidoc89@gmail.com>
2021-09-06 16:47:49 +0200

    il est l'heure

--- js/details.js
+++ js/details.js
@@ -1,6 +1,13 @@
-let url = window.location.href;
+// let url = window.location.href;
 
-let gg = url.substring(url.lastIndexOf('=') + 1);
+// let gg = url.substring(url.lastIndexOf('=') + 1);
 
-console.log("id recuperer 1", gg);
+// console.log("id recuperer 1", gg);
+
+const queryString = window.location.search;
+console.log (queryString);
+const urlParams = new URLSearchParams (queryString);
+
+const product = urlParams .get ('id')
+console.log (product);
 
