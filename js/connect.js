b55c5f30f5d43c792d6c0f7071ae3b65549de7c0
franckonline <vidoc89@gmail.com>
2021-09-06 17:38:17 +0200

    lang=fr

--- js/connect.js
+++ js/connect.js
@@ -4,56 +4,56 @@ let regex = new RegExp("\\S+@\\S+\\.\\S+");
 let champspass = document.querySelector("#inp_pass");
 let mailmail = champsmail.value;
 champsmail.addEventListener("input", checkpass);
-let toutinpok = 0  ;
-function checkpass(){
+let toutinpok = 0;
+
+function checkpass() {
     if (regex.test(champsmail.value)) {
-       
+
         // active button
         bouton.removeAttribute("disabled");
         toutinpok++;
-        console.log("mail ok ?: ",toutinpok);
+        console.log("mail ok ?: ", toutinpok);
     }
-//verif du pass
+    //verif du pass
 }
 
-document.querySelector("button").addEventListener("click", function (){
+document.querySelector("button").addEventListener("click", function () {
     let data = {
         username: champsmail.value,
         password: champspass.value
     }
-    
+
     fetch('http://musics.logikstik.odns.fr/api/login', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
-    })
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
         .then((response) => response.json())
-        
+
         .then((json) => {
             sessionStorage.brouettetouk = json.token;
             console.log("le token est : ", json.token);
             window.location = "../accueil.html";
         });
-    }
-    )
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //  let inscript = document.querySelector(".btn_inscription");
-        //  document.querySelector(".btn_inscription").addEventListener("click", function () {
-        //      window.location.href = "inscription.html";
-        //  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let inscript = document.querySelector(".btn_inscription");
+//  document.querySelector(".btn_inscription").addEventListener("click", function () {
+//      window.location.href = "inscription.html";
+//  });
--- js/details.js
+++ js/details.js
@@ -11,3 +11,21 @@ const urlParams = new URLSearchParams (queryString);
 const product = urlParams .get ('id')
 console.log (product);
 
+let monurl = "http://musics.logikstik.odns.fr/api/albums" + product;
+
+console.log(monurl);
+
+fetch('http://musics.logikstik.odns.fr/api/albums', {
+    method: 'GET',
+    
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+})
+.then((response) => response.json())
+
+.then((json) => {
+    sessionStorage.brouettetouk = json.token;
+    console.log("le token est : ", json.token);
+    window.location = "../accueil.html";
+});
