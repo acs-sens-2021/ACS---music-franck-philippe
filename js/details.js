d54997415eadd0c326d25712c9e71ea5a8099f64
franckonline <vidoc89@gmail.com>
2021-09-07 16:56:51 +0200

    count

--- js/details.js
+++ js/details.js
@@ -77,35 +77,35 @@ fetch(`${monurl}`, {
                             console.log("tracks: " + count + " _piste " + voirtracks[count]);
 
                             // ici debut fetch cherche tracks  *********************
-
+                            let nouvelleurl = `http://musics.logikstik.odns.fr${voirtracks[count]}`;
+                            fetch(nouvelleurl, {
+                                    method: 'GET',
+                                    headers: {
+                                        'Content-type': 'application/json; charset=UTF-8',
+                                        'Authorization': `Bearer ${token}`
+                                    }
+                                })
+                                .then((response) => response.json())
+                                .then((json) => {
+                                    const clone = document.importNode(zonetrack.content, true);
+                                    const spanun = clone.querySelector(".nompiste");
+                                    spanun.textContent = json.name;
+                                    const spantime = clone.querySelector(".timer");
+                                    let timertoto = json.time;
+                                    let resultat = (Math.floor((timertoto/1000)/60));
+                                    let secondes = Math.floor((timertoto - resultat)/60);
+                                    spantime.textContent = resultat + " : " +(secondes/100).toFixed(0);
+                                    divinjection.appendChild(clone);
+    
+                                    // divinjection.clone
+    
+                                });
 
 
                             // ici fin fetch cherche tracks  *********************
                         }
                         ///////////////////////////////
-                        let nouvelleurl = `http://musics.logikstik.odns.fr${voirtracks[0]}`;
-                        fetch(nouvelleurl, {
-                                method: 'GET',
-                                headers: {
-                                    'Content-type': 'application/json; charset=UTF-8',
-                                    'Authorization': `Bearer ${token}`
-                                }
-                            })
-                            .then((response) => response.json())
-                            .then((json) => {
-                                const clone = document.importNode(zonetrack.content, true);
-                                const spanun = clone.querySelector(".nompiste");
-                                spanun.textContent = json.name;
-                                const spantime = clone.querySelector(".timer");
-                                let timertoto = json.time;
-                                let resultat = (Math.floor((timertoto/1000)/60));
-                                let secondes = Math.floor((timertoto - resultat)/60);
-                                spantime.textContent = resultat + " : " +(secondes/100).toFixed(0);
-                                divinjection.appendChild(clone);
-
-                                // divinjection.clone
-
-                            });
+                     
                         ///////////////////////////////
                     });
 
