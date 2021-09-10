12fd59b1eee4a85a90924a9e4e4f115712cba717
franckonline <vidoc89@gmail.com>
2021-09-10 12:58:43 +0200

    ok

--- js/ecoute.js
+++ js/ecoute.js
@@ -1,18 +1,15 @@
 let token = sessionStorage.brouettetouk;
-
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const product = urlParams.get('id');
-
 let image = document.querySelector(".img_source");
 let sonname = document.querySelector(".name");
 let artiste = document.querySelector(".artiste");
 let piste = document.querySelector(".piste");
-console.log("img ",image);
-console.log("name ",  sonname);
+console.log("img ", image);
+console.log("name ", sonname);
 console.log("artiste ", artiste);
 console.log("piste ", piste);
-
 
 let monurl = `http://musics.logikstik.odns.fr/api/tracks/` + product;
 fetch(monurl, {
@@ -24,11 +21,10 @@ fetch(monurl, {
     })
     .then((response) => response.json())
     .then((json) => {
-        console.log("ici console-log ", json);
-
+        
+        piste.textContent = json.name;
 
         // ----- debut deuxieme fetch
-
         let monurl2 = `http://musics.logikstik.odns.fr${json.album}`;
         fetch(monurl2, {
                 method: 'GET',
@@ -39,7 +35,22 @@ fetch(monurl, {
             })
             .then((response) => response.json())
             .then((json) => {
-
+                image.src = json.picture;
+                sonname.textContent = json.name;
+                // troisieme fetch   debut ***************
+                let monurl3 = `http://musics.logikstik.odns.fr${json.artist}`;
+                fetch(monurl3, {
+                        method: 'GET',
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
+                            'Authorization': `Bearer ${token}`
+                        }
+                    })
+                    .then((response) => response.json())
+                    .then((json) => {
+                        artiste.textContent = json.username;
+                    })
+                // troisieme fetch fin **************
             })
 
 
@@ -50,5 +61,5 @@ fetch(monurl, {
         // ----- fin deuxieme fetch
 
 
-  
+
     })
